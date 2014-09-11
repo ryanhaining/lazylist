574a35653c65ae8b216a5237d58c9e69f967dc93
Ryan Haining <rhainin1@binghamton.edu>
2014-09-10 22:51:58 -0400

    testing more stuff

--- tester.py
+++ tester.py
@@ -16,6 +16,15 @@ class TestGetItem(unittest.TestCase):
             lazy[i]
             self.assertEqual(len(lazy._list), i + 1)
         self.assertEqual(len(lazy), range_size)
+        for a, b in zip(lazy._list, range(range_size)):
+            self.assertEqual(a, b)
+            
+
+    def test_all_at_once(self):
+        range_size = 10
+        lazy = lazylist.List(range(range_size))
+        lazy[range_size - 1]
+        self.assertEqual(len(lazy._list), range_size)
 
 
 if __name__ == '__main__':
